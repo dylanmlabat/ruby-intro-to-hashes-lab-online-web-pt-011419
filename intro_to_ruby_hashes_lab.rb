9d5aa8dfa9e003bcdcfb59f291908be10595259c
Dylan Michael Labat <dml252@live.com>
2019-01-24 23:17:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_second_tier
   monopoly = {
 	  railroads: {
 	    pieces: 4,
-	    names: {}
+	    names: {},
 	    rent_in_dollars: {}
 	  }
 	}
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
 	      pennsylvania_railroad: {},
 	      b_and_o_railroad: {},
 	      shortline: {}
-	    }
+	    },
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
 	      two_pieces_owned: 50,
